684524ab8a7684c09a7445abbbc4bfe4a7eea926
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-13 13:03:25 -0600

    Complete exercise 04 and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...values) {
+    
+    for (let i = 0; i < values.length; i++) {
+        if (mainArray.includes(values[i]) == false) {
+            continue;
+        } else {
+            mainArray.splice(mainArray.indexOf(values[i]), 1);
+            console.log(mainArray);
+        } 
+        if (mainArray.includes(values[i])) {
+            i--;
+        }
+    }
+    return mainArray;
 };
 
 // Do not edit below this line
