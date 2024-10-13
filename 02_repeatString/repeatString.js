d7b0f1adb1e68e36cd353866b5856b71218090f5
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-13 03:14:11 -0600

    Complete exercise 02
    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = '';
+    for (let i = 0; i < num; i++) {
+        finalString = finalString.concat(string);
+    }
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return finalString;
+    }
 };
 
 // Do not edit below this line
