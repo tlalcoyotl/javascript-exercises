e438e1e032cf601b1a383d539e5f7d1b222bdf6b
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-13 03:33:03 -0600

    Complete exercise 03 passing all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let chars = string.split('');
+let reverse = ''
+for (i = 0; i < string.length; i++) {
+    reverse = reverse.concat(chars.pop());
+}
+return reverse;
 };
 
 // Do not edit below this line
