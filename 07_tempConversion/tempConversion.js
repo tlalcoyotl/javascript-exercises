b377cb6d3816881299b08290201e6b80ac844082
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-14 23:18:46 -0600

    Complete exercise 07 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    let tempC = parseFloat((tempF - 32) * (5/9));
+    if (Number.isInteger(tempC)) {
+    return parseInt(tempC);
+    } else {
+    return parseFloat(tempC.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = parseFloat((tempC * 9/5) + 32);
+  if (Number.isInteger(tempF)) {
+    return parseInt(tempF);
+  } else {
+  return parseFloat(tempF.toFixed(1));
+  }
 };
 
 // Do not edit below this line
