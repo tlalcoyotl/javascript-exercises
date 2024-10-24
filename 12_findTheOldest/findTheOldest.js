61708fffce4b269218d511eeb42b4263b34a714c
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-24 03:50:34 -0600

    Complete all javascript fundamental exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let currentYear = new Date().getFullYear();
 
+    arr.map(item => {
+        if (typeof item.yearOfDeath == 'undefined') {
+            item.ageOfDeath = currentYear - item.yearOfBirth;
+        } else {
+            item.ageOfDeath = item.yearOfDeath - item.yearOfBirth;
+        }
+    });
+    
+    arr.sort((a,b) => a.ageOfDeath - b.ageOfDeath);
+
+    return arr.pop();
 };
 
 // Do not edit below this line
