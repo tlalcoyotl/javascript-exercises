ffc1495c1e445559c95fd688a463d186c4be6f50
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-24 02:16:27 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let number = parseInt(n);
+    let fibonacciArray = [0, 1];
+    if (number > 1) {
+        for (i = 2; i <= number; i++) {
+            fibonacciArray.push(fibonacciArray[i-2]+fibonacciArray[i-1]);
+        }
+    } else if (number === 0 || number === 1) {
+        return fibonacciArray[number];
+    } else if (number < 0) {
+        return 'OOPS';
+    }
+    return fibonacciArray.pop();
 };
 
 // Do not edit below this line
