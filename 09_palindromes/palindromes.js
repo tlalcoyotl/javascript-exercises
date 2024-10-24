f5864a471c96003e8dfae190290963f26f71f418
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-24 01:36:33 -0600

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
+    let splitString = string.toLowerCase().split('');
+    let cleanArray = splitString.filter(letter => alphanumeric.includes(letter));
+    console.log(cleanArray.join());
+    console.log(cleanArray.reverse().join());
+    return cleanArray.join() === cleanArray.reverse().join();
 };
 
 // Do not edit below this line
